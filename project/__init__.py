cf201ac1d44c593e82dfd72cf106da2f83580b36
Nils Werner <nils.werner@gmail.com>
2014-02-26 12:34:35 +0100

    Including main function into __init__

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,10 @@
-def main():
+from main import main
+
+def main(argv=[]):
     """Entry point for the application script"""
     print("Call your main application code here")
+
+    return main(argv)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
