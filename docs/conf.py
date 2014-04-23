nanaln/python_frft

Cleaned up skeleton for our use

docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# dspy documentation build configuration file, created by
+# frft documentation build configuration file, created by
 # sphinx-quickstart on Sun Apr 20 14:28:17 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'dspy'
+project = 'frft'
 copyright = '2014, International AudioLabs Erlangen'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -183,7 +183,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dspydoc'
+htmlhelp_basename = 'frftdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -203,7 +203,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'dspy.tex', 'dspy Documentation',
+  ('index', 'frft.tex', 'frft Documentation',
    'International AudioLabs Erlangen', 'manual'),
 ]
 
@@ -233,7 +233,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'dspy', 'dspy Documentation',
+    ('index', 'frft', 'frft Documentation',
      ['International AudioLabs Erlangen'], 1)
 ]
 
@@ -247,8 +247,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'dspy', 'dspy Documentation',
-   'International AudioLabs Erlangen', 'dspy', 'One line description of project.',
+  ('index', 'frft', 'frft Documentation',
+   'International AudioLabs Erlangen', 'frft', 'One line description of project.',
    'Miscellaneous'),
 ]
 

setup.py
@@ -39,65 +39,22 @@ class SphinxCommandProxy(setuptools.Command):
                      os.path.join('docs', 'build')])
 
 
-class DevelopCommandProxy(setuptools.command.develop.develop):
-    """
-    `develop` command
-    """
-
-    def run(self):
-        # Download or generate any data that is not part of this
-        # repository but is required for development:
-
-        # urllib.urlretrieve(
-        #     "http://work.audiolabs.uni-erlangen.de/project/input.wav",
-        #     "data/input.wav"
-        # )
-        # urllib.urlretrieve(
-        #     "http://work.audiolabs.uni-erlangen.de/project/input2.wav",
-        #     "data/input2.wav"
-        # )
-        setuptools.command.develop.develop.run(self)
-
-# See if we can use Cython
-USE_CYTHON = False
-
-if "--no-cython" not in sys.argv:
-    try:
-        from Cython.Distutils import build_ext
-        from Cython.Build import cythonize
-        USE_CYTHON = True
-    except ImportError as e:
-        warnings.warn(e.message)
-else:
-    sys.argv.remove("--no-cython")
-
-ext = '.pyx' if USE_CYTHON else '.c'
-
-extension = [
-    setuptools.Extension("project.cython",
-                         ["project/cython" + ext],
-                         include_dirs=[numpy.get_include()]),
-]
-
-if USE_CYTHON:
-    extension = cythonize(extension)
-
 setuptools.setup(
     # Name of the project
-    name='skeleton',
+    name='frft',
 
     # Version
     version='0.1',
 
     # Description
-    description='A skeleton for scientific Python projects.',
+    description='Fractional Fourier transform for NumPy.',
 
     # Your contact information
     author='Nils Werner',
     author_email='nils.werner@gmail.com',
 
     # License
-    license='commercial',
+    license='MIT',
 
     # Packages in this project
     # find_packages() finds all these automatically for you
@@ -109,10 +66,6 @@ setuptools.setup(
         'nose>=1.3.0',
         'numpy>=1.8',
         'scipy>=0.13.0',
-        'matplotlib>=1.3.1',
-        'PyAudio>=0.2.7',
-        'ipdb',
-        'dspy',
         'sphinx',
         'sphinx_rtd_theme'
     ],
@@ -123,28 +76,19 @@ setuptools.setup(
     test_suite="nose.collector",
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Telecommunications Industry',
         'Intended Audience :: Science/Research',
-        'License :: Other/Proprietary License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Multimedia :: Sound/Audio :: Analysis',
         'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis'
     ],
 
-    # Link to dependencies that are not on PyPI, in this case
-    # dspy
-    dependency_links=[
-        'git+https://github.com/nils-werner/dspy.git#egg=dspy'
-    ],
-
     # Register custom commands
     cmdclass={
-        'develop': DevelopCommandProxy,
         'build_sphinx': SphinxCommandProxy
     },
     zip_safe=False,
-    ext_modules=extension
 )

nils-werner/stft

docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# dspy documentation build configuration file, created by
+# stft documentation build configuration file, created by
 # sphinx-quickstart on Sun Apr 20 14:28:17 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'dspy'
+project = 'stft'
 copyright = '2014, International AudioLabs Erlangen'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -183,7 +183,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dspydoc'
+htmlhelp_basename = 'stftdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -203,7 +203,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'dspy.tex', 'dspy Documentation',
+  ('index', 'stft.tex', 'stft Documentation',
    'International AudioLabs Erlangen', 'manual'),
 ]
 
@@ -233,7 +233,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'dspy', 'dspy Documentation',
+    ('index', 'stft', 'stft Documentation',
      ['International AudioLabs Erlangen'], 1)
 ]
 
@@ -247,8 +247,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'dspy', 'dspy Documentation',
-   'International AudioLabs Erlangen', 'dspy', 'One line description of project.',
+  ('index', 'stft', 'stft Documentation',
+   'International AudioLabs Erlangen', 'stft', 'One line description of project.',
    'Miscellaneous'),
 ]
 

setup.py
@@ -1,103 +1,23 @@
-import sys
-import numpy
-import os
-import sphinx
-import sphinx.apidoc
-import urllib
-import warnings
 import setuptools
 import setuptools.command.develop
 
 
-class SphinxCommandProxy(setuptools.Command):
-    user_options = []
-    description = 'sphinx'
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        # metadata contains information supplied in setup()
-        metadata = self.distribution.metadata
-        src_dir = (self.distribution.package_dir or {'': ''})['']
-        src_dir = os.path.join(os.getcwd(),  src_dir)
-
-        # Build docs from docstrings in *.py files
-        sphinx.apidoc.main(
-            ['',
-             '-o', os.path.join('docs', 'source','api'), src_dir])
-
-        # Build the doc sources
-        sphinx.main(['', '-c', 'docs',
-                     '-D', 'project=' + metadata.name,
-                     '-D', 'version=' + metadata.version,
-                     '-D', 'release=' + metadata.version,
-                     os.path.join('docs', 'source'),
-                     os.path.join('docs', 'build')])
-
-
-class DevelopCommandProxy(setuptools.command.develop.develop):
-    """
-    `develop` command
-    """
-
-    def run(self):
-        # Download or generate any data that is not part of this
-        # repository but is required for development:
-
-        # urllib.urlretrieve(
-        #     "http://work.audiolabs.uni-erlangen.de/project/input.wav",
-        #     "data/input.wav"
-        # )
-        # urllib.urlretrieve(
-        #     "http://work.audiolabs.uni-erlangen.de/project/input2.wav",
-        #     "data/input2.wav"
-        # )
-        setuptools.command.develop.develop.run(self)
-
-# See if we can use Cython
-USE_CYTHON = False
-
-if "--no-cython" not in sys.argv:
-    try:
-        from Cython.Distutils import build_ext
-        from Cython.Build import cythonize
-        USE_CYTHON = True
-    except ImportError as e:
-        warnings.warn(e.message)
-else:
-    sys.argv.remove("--no-cython")
-
-ext = '.pyx' if USE_CYTHON else '.c'
-
-extension = [
-    setuptools.Extension("project.cython",
-                         ["project/cython" + ext],
-                         include_dirs=[numpy.get_include()]),
-]
-
-if USE_CYTHON:
-    extension = cythonize(extension)
-
 setuptools.setup(
     # Name of the project
-    name='skeleton',
+    name='stft',
 
     # Version
     version='0.1',
 
     # Description
-    description='A skeleton for scientific Python projects.',
+    description='Fractional Fourier transform for NumPy.',
 
     # Your contact information
     author='Nils Werner',
     author_email='nils.werner@gmail.com',
 
     # License
-    license='commercial',
+    license='MIT',
 
     # Packages in this project
     # find_packages() finds all these automatically for you
@@ -106,15 +26,8 @@ setuptools.setup(
     # Dependencies, this installs the entire Python scientific
     # computations stack
     install_requires=[
-        'nose>=1.3.0',
         'numpy>=1.8',
         'scipy>=0.13.0',
-        'matplotlib>=1.3.1',
-        'PyAudio>=0.2.7',
-        'ipdb',
-        'dspy',
-        'sphinx',
-        'sphinx_rtd_theme'
     ],
 
     tests_require=[
@@ -123,28 +36,14 @@ setuptools.setup(
     test_suite="nose.collector",
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Telecommunications Industry',
         'Intended Audience :: Science/Research',
-        'License :: Other/Proprietary License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Multimedia :: Sound/Audio :: Analysis',
         'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis'
     ],
-
-    # Link to dependencies that are not on PyPI, in this case
-    # dspy
-    dependency_links=[
-        'git+https://github.com/nils-werner/dspy.git#egg=dspy'
-    ],
-
-    # Register custom commands
-    cmdclass={
-        'develop': DevelopCommandProxy,
-        'build_sphinx': SphinxCommandProxy
-    },
     zip_safe=False,
-    ext_modules=extension
 )