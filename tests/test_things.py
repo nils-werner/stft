18c488f98d9934874d41c107a02f642523896a23
Nils Werner <nils.werner@audiolabs-erlangen.de>
2014-10-29 13:00:20 +0100

    100% code coverage

--- tests/test_things.py
+++ tests/test_things.py
@@ -1,5 +1,6 @@
 import stft
 import numpy
+import pytest
 import scipy.fftpack
 
 
@@ -19,29 +20,32 @@ def test_precision():
     Test if transform-inverse identity holds
 
     """
-    siglen = 2048
-    framelen = 512
+    for channels in [1, 2]:
+        for padding in [0, 1, 4]:
+            siglen = 2048
+            framelen = 512
 
-    a = numpy.random.random(siglen)
-    x = stft.spectrogram(a, framelength=framelen)
-    y = stft.ispectrogram(x, framelength=framelen)
+            a = numpy.squeeze(numpy.random.random((siglen, channels)))
+            x = stft.spectrogram(a, framelength=framelen, padding=padding)
+            y = stft.ispectrogram(x, framelength=framelen, padding=padding)
 
-    # Crop first and last frame
-    assert numpy.allclose(a[framelen:-framelen], y[framelen:-framelen])
+            # Crop first and last frame
+            assert numpy.allclose(a[framelen:-framelen], y[framelen:-framelen])
 
 
-def test_multichannel():
-    """
-    Test if multi channel data works
+def test_maxdim():
+    a = numpy.random.random((512, 2, 2))
 
-    """
-    channels = 2
-    siglen = 2048
-    framelen = 512
+    with pytest.raises(ValueError):
+        stft.spectrogram(a)
 
-    a = numpy.random.random((siglen, channels))
-    x = stft.spectrogram(a, framelength=framelen)
-    y = stft.ispectrogram(x, framelength=framelen)
+    b = numpy.random.random((512, 2, 2, 3))
+    with pytest.raises(ValueError):
+        stft.ispectrogram(b)
 
-    # Crop first and last frame
-    assert numpy.allclose(a[framelen:-framelen, :], y[framelen:-framelen, :])
+
+def test_fallback():
+    import scipy.signal
+    del scipy.signal.cosine
+
+    return test_windowlength_errors()
