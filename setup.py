7d79f2b98e42ccd63062c2ab65fea9d097686896
Nils Werner <nils.werner@audiolabs-erlangen.de>
2014-10-29 13:05:42 +0100

    Version 0.4.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         name='stft',
 
         # Version
-        version='0.4.3',
+        version='0.4.4',
 
         # Description
         description='Short Time Fourier transform for NumPy.',
