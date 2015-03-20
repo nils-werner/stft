1edf30fed4b468fba83b1bd908144c01de9b34d3
Nils Werner <nils.werner@audiolabs-erlangen.de>
2015-03-20 14:45:20 +0100

    Removed superfluous dep, fixed sdist tagging

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ if __name__ == "__main__":
                 'pytest',
                 'pytest-cov',
                 'pytest-pep8',
-                'scikits.samplerate',
             ],
         },
 
